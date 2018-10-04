10d19ab6c4e8e527b7f45ddd970c836bcd044abd
Ankit Mishra <ankit.mishra@iisecurity.in>
2018-10-04 12:08:22 +0530

    added aapt to install

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ passwd iis
 echo deb http://mirrors.neusoft.edu.cn/kali kali-rolling main non-free contrib > /etc/apt/sources.list
 echo  -e 'Acquire::http::Proxy "http://192.168.100.102:3142";' >> /etc/apt/apt.conf.d/00aptproxy
 sudo apt-get update
-sudo apt-get install virtualbox arachni vega adb build-essential lxc libssl-dev libreoffice libffi-dev xfce4-screenshooter python-dev jd-gui -y
+sudo apt-get install virtualbox arachni vega adb aapt build-essential lxc libssl-dev libreoffice libffi-dev xfce4-screenshooter python-dev jd-gui -y
 usermod -aG vboxusers iis
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
