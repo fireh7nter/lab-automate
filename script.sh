acb2214fa8ced5fe911c1031c280b49694e7f7da
Ankit Mishra <ankit.mishra@iisecurity.in>
2018-10-04 12:05:29 +0530

    added script.sh for initial install in kali-xfce

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+useradd -m iis
+usermod -aG sudo iis
+chsh -s /bin/bash iis
+passwd iis
+echo deb http://mirrors.neusoft.edu.cn/kali kali-rolling main non-free contrib > /etc/apt/sources.list
+echo  -e 'Acquire::http::Proxy "http://192.168.100.102:3142";' >> /etc/apt/apt.conf.d/00aptproxy
+sudo apt-get update
+sudo apt-get install virtualbox arachni vega adb build-essential lxc libssl-dev libreoffice libffi-dev xfce4-screenshooter python-dev jd-gui -y
+usermod -aG vboxusers iis
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
