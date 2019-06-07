348867793f4603afc69b04713ca3b410d844618c
Fire Hunter <ankit.mishra@iisecurity.in>
2019-06-07 16:34:53 +0530

    changed the mirror to kali.cs.nctu.edu.tw

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ useradd -m iis
 usermod -aG sudo iis
 chsh -s /bin/bash iis
 passwd iis
-echo deb http://mirrors.neusoft.edu.cn/kali kali-rolling main non-free contrib > /etc/apt/sources.list
+echo deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib > /etc/apt/sources.list
+echo deb-src http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib > /etc/apt/sources.list
 echo  -e 'Acquire::http::Proxy "http://192.168.100.102:3142";' >> /etc/apt/apt.conf.d/00aptproxy
 sudo apt-get update
 sudo apt-get install virtualbox arachni adb aapt build-essential lxc libssl-dev libreoffice libffi-dev xfce4-screenshooter python-dev jd-gui -y
