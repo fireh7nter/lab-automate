70b36f2db1a8fae8bc0d448cb0b5ba95f5147a1a
Fire Hunter <fireh7nter@fireh7nter.com>
2019-06-12 11:10:29 +0530

    removed deb-src

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ usermod -aG sudo iis
 chsh -s /bin/bash iis
 passwd iis
 echo deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib > /etc/apt/sources.list
-echo deb-src http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib > /etc/apt/sources.list
+#echo deb-src http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib > /etc/apt/sources.list
 echo  -e 'Acquire::http::Proxy "http://192.168.100.102:3142";' >> /etc/apt/apt.conf.d/00aptproxy
 sudo apt-get update
 sudo apt-get install virtualbox arachni adb aapt build-essential lxc libssl-dev libreoffice libffi-dev xfce4-screenshooter python-dev jd-gui -y
